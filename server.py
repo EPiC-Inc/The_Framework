d5e24ffcec1b3b695443c1f0e77435a10c38ad81
Xikun <emmettwb@gmail.com>
2018-08-23 20:58:41 -0600

    started starting

--- server.py
+++ server.py
@@ -1,13 +1,33 @@
-#imports
-from flask import Flask, render_template
+# Imports
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit
+import json, hashlib, random
 # For PyInstaller:
 import asyncio, engineio.async_threading, jinja2
 
+# Variables
 app = Flask(__name__)
-socketio = SocketIO(app)
+io = SocketIO(app)
 
-#routing
+clients=[]
+
+# Functions
+def randomize_targets(list_of_names):
+    ''' Returns a dict of names such that randomize(list)[name] = name's target '''
+    final = {}
+    random.shuffle(list_of_names)
+    for name in list_of_names:
+        if list_of_names.index(name) == (len(list_of_names) - 1):
+            i = 0
+        else:
+            i = (list_of_names.index(name) + 1)
+        final[name] = list_of_names[i]
+    return final
+
+def hash(data):
+    return hashlib.md5(str(data).encode('utf-8')).hexdigest()
+
+# Routing
 @app.route("/") #home
 def index():
    return render_template("index.html")
@@ -20,6 +40,21 @@ def return_html(filename):
 def handler(e):
     return render_template('500.html').format(e)
 
+# Socket.io events
+@io.on('connect')
+def connected():
+    pass
+
+@io.on('disconnect')
+def disconnect():
+    pass
+
+@io.on('test')
+def return_test():
+    print('hi')
+    emit('a-ok') # Emits *to client*
+
+# Main
 if __name__ == '__main__':
    app.run()
 
--- static/index.js
+++ static/index.js
@@ -1,3 +1,20 @@
-function login() {
-  console.log('test');
+var socket = io();
+
+show_login_button = document.getElementById('show_login_button');
+login_div = document.getElementById('login_div');
+username = document.getElementById('userbox');
+password = document.getElementById('pwdbox');
+
+function show_login() {
+  show_login_button.style.opacity = 0;
+  login_div.style.opacity = 1;
 }
+
+function login() {
+  console.log('logging in...');
+  socket.emit('auth', [username.value, password.value]);
+}
+
+socket.on('a-ok', function(){
+  console.log('a-ok recieved');
+});
